c5f4a4fe907c5150be3fa653485dbf591f291050
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-29 14:26:36 -0300

    Organizando arquivos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Personal Blog`,
+    description: `A blog about fullstack development and other coll stuff`,
+    author: `@wslmacieira`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
