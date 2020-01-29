46f63deed898be74d67810a47201aa6411cb716a
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-29 18:30:00 -0300

    Compartilhando component entre páginas

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Profile = () => (
+  <div className="profile-wrapper">
+    <h1>Wagner dos Santos</h1>
+    <h2>Desenvolvedor Front-End</h2>
+    <p>Desenvolvedor | ReactJS | React Native | Node.js</p>
+  </div>
+);
+
+export default Profile;
