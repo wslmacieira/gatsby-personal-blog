ebc2cce61739186c4cc56157d0b489dd6ba8536b
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-30 17:21:39 -0300

    Criando SocialLinks component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/wslmacieira'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com'
+  },
+  {
+    label: 'Youtube',
+    url: 'https://www.youtube.com'
+  },
+  {
+    label: 'Instagran',
+    url: 'https://www.instagran.com'
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/wagner-santos-lima-60691a192/'
+  },
+]
+
+export default links;
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,15 @@
+import { Github } from 'styled-icons/boxicons-logos/Github';
+import { Twitter} from 'styled-icons/boxicons-logos/Twitter';
+import { Youtube} from 'styled-icons/boxicons-logos/Youtube';
+import { Instagran } from 'styled-icons/boxicons-logos/Instagram';
+import { Linkedin } from 'styled-icons/boxicons-logos/LinkedinSquare';
+
+const Icons = {
+  Github,
+  Twitter,
+  Youtube,
+  Instagran,
+  Linkedin,
+}
+
+export default Icons;
