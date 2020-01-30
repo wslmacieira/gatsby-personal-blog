bff46b3130479eb4a90bfa62d6c5a21dd15a1008
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-30 13:32:57 -0300

    Refatorando o styled do component Layout

--- src/components/_Layout/index.js
+++ src/components/_Layout/index.js
@@ -2,20 +2,18 @@ import React from "react"
 import PropTypes from "prop-types"
 import Profile from '../Profile';
 
-import { LayoutWrapper, LayoutMain } from './styles';
-
+import * as S from './styles';
 import GlobalStyles from '../../styles/global';
 
 const Layout = ({ children }) => {
-
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
       <aside>
         <Profile />
       </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
