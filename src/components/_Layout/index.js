a0830a79cde84fce27ef7b09f5bc30a8afa2c0b7
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-30 13:28:41 -0300

    Criando estilos globais com createGlobalStyle

--- src/components/_Layout/index.js
+++ src/components/_Layout/index.js
@@ -4,10 +4,13 @@ import Profile from '../Profile';
 
 import { LayoutWrapper, LayoutMain } from './styles';
 
+import GlobalStyles from '../../styles/global';
+
 const Layout = ({ children }) => {
 
   return (
     <LayoutWrapper>
+      <GlobalStyles />
       <aside>
         <Profile />
       </aside>
