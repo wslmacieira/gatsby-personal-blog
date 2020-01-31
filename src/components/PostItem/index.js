a9c305eb86c87afdc3d8dee860fc0ca488654acc
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-31 12:21:38 -0300

    Estruturando os dados do PostItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as S from './styles';
 
-const PostItem = () => (
-  <S.PostItemLink>
+const PostItem = ({slug, background, category, date, timeToRead, title, description}) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#47650b">Misc</S.PostItemTag>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
-        <S.PostItemDate>31 de Janeiro de 2020 * 4 min de leitura</S.PostItemDate>
-        <S.PostItemTitle>Diga não ao Medium: tenha sua própria plataforma</S.PostItemTitle>
-        <S.PostItemDescription>Algumas razões para voçê ter sua propria plataforma ao invés de
-          soluções como o Medium.
+        <S.PostItemDate>{date} * {timeToRead} min de leitura</S.PostItemDate>
+<S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}
         </S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+
+}
 
 export default PostItem;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,14 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
+    <PostItem
+    slug="/about/"
+    category="Misc"
+    date="31 de Janeiro de 2020"
+    timeToRead="5"
+    title="Diga não ao Medium: tenha sua própria plataforma"
+    description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
   </Layout>
 )
 
