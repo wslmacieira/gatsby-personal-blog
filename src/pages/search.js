2dd891f6f1657d0ed1edab673ef8d77bb6ab7a76
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-02-01 18:58:33 -0300

    Criando a página Search

---
+++ src/pages/search.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/_Layout"
+import SEO from "../components/seo"
+import Search from '../components/Search';
+
+const SearchPage = () => (
+  <Layout>
+    <SEO title="Search" />
+    <Search />
+  </Layout>
+)
+
+export default SearchPage;
