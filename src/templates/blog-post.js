56bb1909921d07dd2ff6508d651a095c8c821ef6
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-31 23:54:57 -0300

    Integrando Layout ao Blog Post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Layout from "../components/_Layout";
+import SEO from "../components/seo";
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark;
 
   return (
-    <>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    </Layout>
   )
 }
 
